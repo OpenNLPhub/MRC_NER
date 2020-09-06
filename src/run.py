59ac80f80cd17b5e53a232bd884ceb7579dcb20b
Zreal <zenglz_pro@163.com>
2020-09-06 11:07:59 +0800

    finish start moduel

--- src/run.py
+++ src/run.py
@@ -6,11 +6,14 @@
 '''
 
 import os
-from src.data.dataloader import create_MRC_DataLoader,create_Bert_tokenizer
+from src.data.dataloader import create_MRC_DataLoader,create_Bert_tokenizer,\
+    create_HBT_DataLoader
 from src.model.wrapper.MRCBert_NER import MRCBert_NER
+from src.model.wrapper.HBT import hbtModel
 import src.config.args as args
 from src.metrics.metrics import convert_units_to_dataframe
 from src.utils.common import save_model,load_model
+
 
 def run_Bert_MRC_NER():
 
@@ -30,3 +33,30 @@ def run_Bert_MRC_NER():
     units=model.test(test_data_loader,tokenizer=tokenizer,label_class=args.MRC_TAG)
     df=convert_units_to_dataframe(units)
     df.to_csv(args.MRC_RESULT_PATH)
+
+def run_HBT():
+    model_is_existed=os.path.exist(args.HBT_MODEL_PATH)
+    path=os.path.join(args.HBT_SOURCE_DATA,'triple')
+    tokenizer=create_Bert_tokenizer(True,model='bert-base-uncased')
+    if not model_is_existed:
+        train_data_loader=create_HBT_DataLoader('train',path,tokenizer)
+        dev_data_loader=create_HBT_DataLoader('dev',path,tokenizer)
+        relation_list=[]
+        with open(os.path.join(path,'relation_type.txt'),'r') as f:
+            for line in f.readlines():
+                relation_list.append(line.strip()) 
+        model=hbtModel(len(relation_list,use_pretrained=True,model='vert-base-uncased'))
+        model.train(train_data_loader,dev_data_loader)
+        save_model(model,args.HBT_MODEL_PATH)
+    else:
+        model=load_model(args.MRC_MODEL_PATH)
+    test_data_loader=create_HBT_DataLoader('test',path,tokenizer)
+    units=model.test(test_data_loader,tokenizer=tokenizer)
+    df=convert_units_to_dataframe(units)
+    df.to_csv(args.HBT_RESULT_PATH)
+
+    
+
+
+
+    
